6c22bf201edf892ab2e234da8e633fe3f417350f
Salah Ashraf <salahashraf924@gmail.com>
2023-10-03 20:48:23 +0300

    chore: fix style issue

--- source/Packet/Packet.cpp
+++ source/Packet/Packet.cpp
@@ -1,51 +1,55 @@
 #include "../../header/Packet/Packet.h"
 
-void Packet::setPacketLine( std::string packetLine)
+void Packet::setPacketLine(std::string packetLine)
 {
     this->packetLine = packetLine;
 }
 
-void Packet::setSrcAddr( std::string srcAddr)
+void Packet::setSrcAddr(std::string srcAddr)
 {
     this->srcAddr = srcAddr;
 }
 
-void Packet::setDstAddr( std::string dstAddr)
+void Packet::setDstAddr(std::string dstAddr)
 {
     this->dstAddr = dstAddr;
 }
 
-void Packet::setType( std::string type)
+void Packet::setType(std::string type)
 {
     this->type = type;
 }
 
-void Packet::setCrc( std::string crc)
+void Packet::setCrc(std::string crc)
 {
     this->crc = crc;
 }
+void Packet::setRawPacket(std::string rawPacket)
+{
+    this->rawPacket = rawPacket;
+}
 
-std::string Packet::getPacketLine() 
+std::string Packet::getPacketLine()
 {
     return packetLine;
 }
 
-std::string Packet::getSrcAddr() 
+std::string Packet::getSrcAddr()
 {
     return srcAddr;
 }
 
-std::string Packet::getDstAddr() 
+std::string Packet::getDstAddr()
 {
     return dstAddr;
 }
 
-std::string Packet::getType() 
+std::string Packet::getType()
 {
     return type;
 }
 
-std::string Packet::getCrc() 
+std::string Packet::getCrc()
 {
     return crc;
 }
