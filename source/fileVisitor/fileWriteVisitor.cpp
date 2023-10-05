4df3b877e395bc14d20602fbfd24c03239eaed03
Ruqaiyah Mohammed <77598596+25Ruq@users.noreply.github.com>
2023-10-05 22:23:33 +0300

    fix: fix naming bugs

--- source/fileVisitor/fileWriteVisitor.cpp
+++ source/fileVisitor/fileWriteVisitor.cpp
@@ -9,23 +9,23 @@ FileWriteVisitor::FileWriteVisitor(std::string fileName)
 void FileWriteVisitor::writePacket(EthernetPacket* ethpacket)
 {
 	fileStream << ethpacket->getRawPacket() << "\n";
-	fileStream << "CRC: " << ethpacket->getCrc() << "\n";
-	fileStream << "Destination Address: " << ethpacket->getDstAddr() << "\n";
-	fileStream << "Source Address: " << ethpacket->getSrcAddr() << "\n";
+	fileStream << "CRC: " << ethpacket->getCRC() << "\n";
+	fileStream << "Destination Address: " << ethpacket->getDestinationAddress() << "\n";
+	fileStream << "Source Address: " << ethpacket->getSourceAddress() << "\n";
 	fileStream << "Type: " << ethpacket->getType() << "\n";
 }
 
 void FileWriteVisitor::writePacket(EcpriPacket* ecpripacket)
 {
 	fileStream << ecpripacket->getRawPacket() << "\n";
-	fileStream << "CRC: " << ecpripacket->getCrc() << "\n";
+	fileStream << "CRC: " << ecpripacket->getCRC() << "\n";
 	fileStream << "Concatenation Indicator: " << ecpripacket->getConcatenationIndicator() << "\n";
-	fileStream << "Destination Address: " << ecpripacket->getDstAddr() << "\n";
-	fileStream << "Message Type: " << ecpripacket->getMsgType() << "\n";
+	fileStream << "Destination Address: " << ecpripacket->getDestinationAddress() << "\n";
+	fileStream << "Message Type: " << ecpripacket->getMessageType() << "\n";
 	fileStream << "Payload Size: " << ecpripacket->getPayloadSize() << "\n";
-	fileStream << "Protocol Version: " << ecpripacket->getProtocolVer() << "\n";
+	fileStream << "Protocol Version: " << ecpripacket->getProtocolVersion() << "\n";
 	fileStream << "RTC ID: " << ecpripacket->getRtcId() << "\n";
-	fileStream << "Sequence ID: " << ecpripacket->getSeqId() << "\n";
-	fileStream << "Source Address: " << ecpripacket->getSrcAddr() << "\n";
+	fileStream << "Sequence ID: " << ecpripacket->getSequenceId() << "\n";
+	fileStream << "Source Address: " << ecpripacket->getDestinationAddress() << "\n";
 	fileStream << "Type: " << ecpripacket->getType() << "\n";
 }
